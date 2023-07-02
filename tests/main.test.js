17971ce6ec07b01e02b6c26d5b30310800b79276
Yuqing Wang <bibolikepeaches@gmail.com>
2023-07-03 00:01:59 +0100

    delete test added

--- tests/main.test.js
+++ tests/main.test.js
@@ -16,8 +16,8 @@ test("Submitting a new task adds it to the list", () => {
 )
 
 test("Checking an entry marks it as complete", () => {
-  // I think we should check if taskData is updated
-  
+  // Todo: we should check if taskData is updated
+
   // click on the first checkbox
   const firstCheckbox = taskList.querySelector("input[type=checkbox]");
 
@@ -28,5 +28,24 @@ test("Checking an entry marks it as complete", () => {
   const expected = true;
 
   equal(actual, expected, "first checkbox should be checked");
+
+});
+
+test("Deleting an entry removes it from the list", () => {
+
+  // adding a new task
+  const expected = "Eat Dinner";
+  toDo.value = expected;
+  date.value = "2023-07-07";
+  var Event = new MouseEvent("click");
+  addButton.dispatchEvent(Event);
+
+  // click on the last delete button
+  const lastDeleteButton = taskList.lastChild.querySelector(".delete-button button");
+  lastDeleteButton.click();
+
+  const actual = taskList.lastChild.querySelector(".description-column").textContent;
+
+  notEqual(actual, expected, "new task should be deleted");
 
 });
